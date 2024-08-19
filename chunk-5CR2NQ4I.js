4ce3e7e15ed553e9b526029c612ee36b6aa52f94
Nazar Maidan <nazar_maydan@ukr.net>
2024-08-19 21:35:33 +0300

    Auto-generated commit

---
+++ chunk-5CR2NQ4I.js
@@ -0,0 +1,1 @@
+import{Q as o,qa as a,ra as i,ua as r,wa as s}from"./chunk-N4KANRZK.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-about"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,c){n&1&&(a(0,"p"),r(1,"about works!"),i())}});let t=e;return t})();export{d as AboutComponent};
---
+++ chunk-NRJ33QH4.js
@@ -0,0 +1,1 @@
+import{Q as o,qa as a,ra as i,ua as r,wa as s}from"./chunk-N4KANRZK.js";var d=(()=>{let t=class t{};t.\u0275fac=function(n){return new(n||t)},t.\u0275cmp=o({type:t,selectors:[["app-contacts"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,p){n&1&&(a(0,"p"),r(1,"contacts works!"),i())}});let e=t;return e})();export{d as ContactsComponent};
